7a43813e15022fb5e15449fbfbcb566979253383
martinschulz <martinschulzofficial@gmail.com>
2022-05-10 22:06:44 +0500

    add i18n

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
 
+import { useTranslation } from 'react-i18next';
+
 import styles from './Header.module.scss';
 
 
 export const Header = () => {
+  const { t } = useTranslation();
+
   return (
     <header className={styles.header}>
       <nav>
         <ul className={styles.menu}>
-          <li><a href="/">Прогнозы</a></li>
-          <li><a href="/">Расписание</a></li>
-          <li><a href="">Сотрудники</a></li>
-          <li><a href="">Отчёты</a></li>
-          <li><a href="">Настройки</a></li>
+          <li><a href="/">{t('header.menu.forecast')}</a></li>
+          <li><a href="/">{t('header.menu.schedule')}</a></li>
+          <li><a href="">{t('header.menu.employees')}</a></li>
+          <li><a href="">{t('header.menu.reports')}</a></li>
+          <li><a href="">{t('header.menu.settings')}</a></li>
         </ul>
       </nav>
     </header>
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,32 @@ import './index.module.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: {
+          'header.menu.forecast': 'forecast',
+          'header.menu.schedule': 'schedule',
+          'header.menu.employees': 'employees',
+          'header.menu.reports': 'reports',
+          'header.menu.settings': 'settings',
+        }
+      }
+    },
+    lng: "en",
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false
+    }
+  }).then(() => console.log('i18n done'));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
